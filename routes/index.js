afe6442e5cfd04f6775a8bd2693c88a621dd8da1
holaleche <whoisleche@gmail.com>
2024-08-18 21:10:28 +0900

    Refactor: export 구문 수정

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { renderBlocks } = require("../controllers/render");
+const renderBlocks = require("../controllers/render");
 
 router.get("/", renderBlocks);
 
