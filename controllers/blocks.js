bb0b6fb6879d5524661158c5d0d9380d4b9d64a3
holaleche <whoisleche@gmail.com>
2024-08-27 20:35:50 +0900

    Refactor: 코드 템플릿, 변수명 및 함수명 변경

--- controllers/blocks.js
+++ controllers/blocks.js
@@ -1,27 +1,35 @@
 const createError = require("http-errors");
 
-const convertBlocksToNaturalLanguage = require("../utils/convertBlocks");
-const convertNaturalLanguageToCodes = require("../utils/convertLanguage");
-const assemblePlaywrightTestCodes = require("../utils/assembleTestCodes");
+const processNaturalLanguage = require("../services/api");
+const { processLineBlocks, processTestCodes } = require("../utils/utils");
+
+const TEST_CODE_TEMPLATE = `
+const { chromium } = require("playwright");
+(async () => {
+  try {
+    %TEST_CODE%
+    await browser.close();
+  } catch (error) {
+    console.error(error);
+  }
+})();
+`.trim();
 
 const handleBlocks = async (req, res, next) => {
   try {
-    const lineBlocks = req.body;
+    const { body: lineBlocks } = req;
 
-    const naturalLanguage = convertBlocksToNaturalLanguage(lineBlocks);
-    const playwrightTestCode =
-      await convertNaturalLanguageToCodes(naturalLanguage);
-    const testCodes = assemblePlaywrightTestCodes(playwrightTestCode);
+    const naturalLanguage = processLineBlocks(lineBlocks);
+    const playwrightCode = await processNaturalLanguage(naturalLanguage);
+    const playwrightTestCodes = processTestCodes(playwrightCode);
 
-    const startText = `const { chromium } = require("playwright");\n(async() => { \n  try { \n`;
-    const endText =
-      "\n    await browser.close();   } catch (error) { \n    console.error(error);   } \n})();";
-
-    const formattedTestCodes = startText + testCodes + endText;
+    const formattedTestCodes = TEST_CODE_TEMPLATE.replace(
+      "%TEST_CODE%",
+      playwrightTestCodes,
+    );
 
     res.json({ formattedTestCodes });
   } catch (error) {
-    console.error(error);
     next(createError(500, "Server Error"));
   }
 };
